08238944d9bce178711a1f52e3b45dec9911ddf2
Mahiuddin_Shamim <mahiuddins27@gmail.com>
2020-11-14 23:15:50 -0500

    test push

--- src/odyssey/java/assignments/loops/W3_For_Loop/Exercise_19.java
+++ src/odyssey/java/assignments/loops/W3_For_Loop/Exercise_19.java
@@ -1,4 +1,28 @@
 package src.odyssey.java.assignments.loops.W3_For_Loop;
 
+import java.util.Scanner;
+
 public class Exercise_19 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i,n;
+        float s= 0;
+        System.out.println("Input the number of terms : ");
+        n = scan.nextInt();
+        System.out.println("\n\n");
+//        for(i=1;i<=n;i++)
+//        {
+//            if(i<n)
+//            {
+//                printf("1/%d + ",i);
+//                s+=1/(float)i;
+//            }
+//            if(i==n)
+//            {
+//                printf("1/%d ",i);
+//                s+=1/(float)i;
+//            }
+//        }
+//        printf("\nSum of Series upto %d terms : %f \n",n,s);
+    }
 }
